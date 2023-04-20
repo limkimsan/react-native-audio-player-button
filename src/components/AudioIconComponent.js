b9772f10f9b5ec75006c418a5fdcbe96c33396e5
Kimsanlim <kimsanlim27@gmail.com>
2023-04-20 12:02:35 +0700

    Show a strike line on the disabled play icon

--- src/components/AudioIconComponent.js
+++ src/components/AudioIconComponent.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {View} from 'react-native'
 
 import color from '../constants/color_constant';
 import {defaultIconSize, primaryIconColor, secondaryIconColor} from '../constants/component_constant';
@@ -13,13 +14,24 @@ const AudioIconComponent = (props) => {
 
   const iconSize = props.iconSize || defaultIconSize
 
+  const renderStrikeLine = () => {
+    return <View style={{height: 3, width: iconSize, transform: [{rotate: '45deg'}], backgroundColor: 'white', justifyContent: 'center', position: 'absolute', top: iconSize / 2.4, left: -2, zIndex: 10}}>
+              <View style={{height: 1.5, backgroundColor: color.muted}} />
+           </View>
+  }
+
   {/* CloneElement is used so we can pass different type of icon and still using the same configuration */}
   return (
-    React.cloneElement(audioIconHelper.getIcon(props.customIcon, props.isSpeakerIcon), {
-      name: audioIconHelper.getIconName(props.customIconSet, props.isSpeakerIcon, props.audio, props.isPlaying),
-      size: iconSize, color: !!props.audio ? getIconColor() : color.muted,
-      style: [props.iconStyle, { width: iconSize, marginLeft: (props.isPlaying  && !props.isSpeakerIcon) ? -2 : 0 }]
-    })
+    <View>
+      {(!props.audio && !props.isSpeakerIcon && !props.customIcon) && renderStrikeLine()}
+      {
+        React.cloneElement(audioIconHelper.getIcon(props.customIcon, props.isSpeakerIcon), {
+          name: audioIconHelper.getIconName(props.customIconSet, props.isSpeakerIcon, props.audio, props.isPlaying),
+          size: iconSize, color: !!props.audio ? getIconColor() : color.muted,
+          style: [{ width: iconSize }, audioIconHelper.getIconMargin(props.isPlaying, props.isSpeakerIcon, !!props.customIcon), props.iconStyle]
+        })
+      }
+    </View>
   )
 }
 
